b913669965654127af05e2dc4a61ec457a95a8cd
Carly Donais <carly.donais@gmail.com>
2022-05-23 14:16:28 -0400

    set past, present, future

--- develop/script.js
+++ develop/script.js
@@ -1,73 +1,130 @@
 var currentDay=document.getElementById("currentDay")
-var todaysDate=moment().format('MMM DD, YYYY');
-var currentTime=moment().format('h A');
+var todaysDate=moment().format('dddd, MMMM Do, YYYY');
+var currentTime=moment().format("h A");
 var timeBlock=document.querySelector(".time-block");
-var timeBlockText=document.querySelector("textarea")
-var hour=document.querySelectorAll(".hour");
-var time=document.getElementById("time");
-var time9=document.getElementById("time9");
-var time10=document.getElementById("time10");
-var time11=document.getElementById("time11");
-var time12=document.getElementById("time12");
-var time1=document.getElementById("time1");
-var time2=document.getElementById("time2");
-var time3=document.getElementById("time3");
-var time4=document.getElementById("time4");
-var time5=document.getElementById("time5");
 var saveButtton=document.getElementById("saveButton");
 var newEvent="";
+var textArea9=document.getElementById("textArea9");
+var textArea10=document.getElementById("textArea10");
+var textArea11=document.getElementById("textArea11");
+var textArea12=document.getElementById("textArea12");
+var textArea1=document.getElementById("textArea1");
+var textArea2=document.getElementById("textArea2");
+var textArea3=document.getElementById("textArea3");
+var textArea4=document.getElementById("textArea4");
+var textArea5=document.getElementById("textArea5");
+var time9=moment().set('hour',9).format("h A");
+var time10=moment().set('hour',10).format("h A");
+var time11=moment().set('hour',11).format("h A");
+var time12=moment().set('hour',12).format("h A");
+var time1=moment().set('hour',13).format("h A");
+var time2=moment().set('hour',14).format("h A");
+var time3=moment().set('hour',15).format("h A");
+var time4=moment().set('hour',16).format("h A");
+var time5=moment().set('hour',17).format("h A");
+
 
 
 console.log(currentDay);
 console.log(currentTime);
+console.log(time9);
+console.log(time1);
 
 function showTodaysDate(){
     currentDay.innerHTML=(todaysDate);
 }
-
-
-function setTimeBlocks(){
-            time9.innerHTML=moment().set("hour", 9).format("h A")
-            time10.innerHTML=moment().set("hour", 10).format("h A");
-            time11.innerHTML=moment().set("hour", 11).format("h A");
-            time12.innerHTML=moment().set("hour", 12).format("h A");
-            time1.innerHTML=moment().set("hour", 1).format("h A");
-            time2.innerHTML=moment().set("hour", 2).format("h A");
-            time3.innerHTML=moment().set("hour", 3).format("h A");
-            time4.innerHTML=moment().set("hour", 4).format("h A");
-            time5.innerHTML=moment().set("hour", 5).format("h A");
-        console.log(hour);
     
-
 
      //if hour is current time set to present
-     if (moment(currentTime, "h A").isBefore(hour, "h A")){
-         $(timeBlockText).addClass("past");
-     }
-     else if (hour === moment(currentTime, "h A")){
-        $(timeBlockText).addClass("present");
-    }
-    //if hour is later than current time set to future
-     else {
-       $(timeBlockText).addClass("future");
-    }
-    //if hour is earlier than current time, set to past
-    
-    //create calendar event
-  //  function createEvent(){
-    //    newEvent.innerHTML=(timeBlockText);
-    //}
-    
-    //save calendar event
-      //  function saveEvent(){
-           
-        //        localStorage.setItem(newEvent, JSON.stringify(newEvent))
+     if (currentTime > time9){
+      $("#textArea9").addClass("future");
+  }
+     else if (time9 === currentTime){
+       $("#textArea9").addClass("present");
+   }
+   else {
+       $("#textArea9").addClass("past");
+   }
 
-          //      console.log(newEvent);
-        //}
-        
-    
+
+     if (currentTime > time10){
+         $("#textArea10").addClass("past");
+     }
+     else if (time10 === currentTime){
+        $("#textArea10").addClass("present");
     }
- //saveButtton.addEventListener('click', saveEvent);   
+    else {
+        $("#textArea10").addClass("future");
+    }
+
+    if (currentTime > time11){
+        $("#textArea11").addClass("past");
+    }
+    else if (time11 === currentTime){
+       $("#textArea11").addClass("present");
+   }
+   else {
+       $("#textArea11").addClass("future");
+   }
+
+
+   if (currentTime > time12){
+    $("#textArea12").addClass("past");
+}
+    else if (time12 === currentTime){
+   $("#textArea12").addClass("present");
+}
+    else {
+   $("#textArea12").addClass("future");
+};
+if (currentTime > time1){
+ $("#textArea1").addClass("past");
+}
+else if (time1 === currentTime){
+   $("#textArea1").addClass("present");
+}
+else {
+   $("#textArea1").addClass("future");
+};
+if (currentTime > time2){
+    $("#textArea2").addClass("past");
+}
+else if (time2 === currentTime){
+   $("#textArea2").addClass("present");
+}
+else {
+   $("#textArea2").addClass("future");
+};
+if (currentTime > time3){
+    $("#textArea3").addClass("past");
+}
+else if (time3 === currentTime){
+   $("#textArea3").addClass("present");
+}
+else {
+   $("#textArea3").addClass("future");
+};
+if (currentTime > time4){
+    $("#textArea4").addClass("past");
+}
+else if (time4 === currentTime){
+   $("#textArea4").addClass("present");
+}
+else {
+   $("#textArea4").addClass("future");
+};
+if (currentTime > time5){
+    $("#textArea5").addClass("past");
+}
+else if (time5 === currentTime){
+   $("#textArea5").addClass("present");
+}
+else {
+   $("#textArea5").addClass("future");
+}
+
+
+    
+
 showTodaysDate();
-setTimeBlocks();
+//setTimeBlocks();
